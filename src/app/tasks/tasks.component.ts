e51a0f02a03e6005f6079ed50f76b78d8d479f10
Jamin Quimby <jamin.quimby@blackbaud.me>
2017-10-19 10:29:49 -0400

    code cleanup

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,59 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Component } from '@angular/core';
 import { SkyModalService, SkyModalCloseArgs } from '@blackbaud/skyux/dist/core';
 import { TaskModel } from './task.model';
 import { TaskFormComponent } from './task-form.component';
-import * as toolbox from 'sw-toolbox';
-import { AuthService } from '../shared/auth.service';
-
+import { TasksService } from './tasks.service';
 @Component({
   selector: 'uapi-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['tasks.component.scss']
 })
 
-export class TaskComponent implements OnInit {
-  // public tasks: AngularFireList<Array<firebase.database.DataSnapshot>>;
-  public tasksCollection: AngularFirestoreCollection<TaskModel>;
-  public taskView: BehaviorSubject<Array<TaskModel>> = new BehaviorSubject([]);
-
+export class TaskComponent {
   constructor(
-    private auth: AuthService,
-    private db: AngularFirestore,
-    private modal: SkyModalService) {
-
-    // Setup SW Toolbox - verbose.
-    toolbox.options.debug = false;
-    toolbox.router.post('(.*)', toolbox.fastest);
-    toolbox.router.get('/tasks(.*)', toolbox.fastest, {
-      debug: false,
-      networkTimeoutSeconds: 4,
-      cache: {
-        name: 'task-cache-v1',
-        maxEntries: 10,
-        maxAgeSeconds: 200
-      }
-    });
-  }
-
-  public ngOnInit() {
-    this.auth.org$.subscribe(org => {
-      // setup database
-      this.tasksCollection = this.db.collection<TaskModel>('/organizations/' + org.id + '/tasks/');
-      // does not contain ids //  this.tasks$ = this.tasksCollection.valueChanges();
-
-      this.tasksCollection.snapshotChanges().map(actions => {
-        return actions.map(action => {
-          const data = action.payload.doc.data() as TaskModel;
-          const id = action.payload.doc.id;
-          return { id, ...data };
-        });
-      }).subscribe((data) => {
-        this.updateView(data, true);
-      });
-    });
-  }
+    private service: TasksService,
+    private modal: SkyModalService) { }
 
   // Skyux Modal with a form inside.
   public openModal(task?: TaskModel) {
@@ -65,28 +24,11 @@ export class TaskComponent implements OnInit {
       }
     };
     // Make a modal Instance
-    let modalInstance = this.modal.open(TaskFormComponent, windowMode['defaultModal']);
-    modalInstance.closed.subscribe((result: SkyModalCloseArgs) => {
-      this.save(Object.assign(new TaskModel(), result.data));
-    });
-  }
-  protected remove(id: string) {
-    this.tasksCollection.doc(id).delete();
-  }
-  private save(task: TaskModel) {
-    if (task.id) {
-      this.tasksCollection.doc(task.id).update(Object.assign({}, task));
-    } else {
-      this.tasksCollection.add(Object.assign({}, task));
-    }
-  }
-
-  private updateView(tasks: Array<TaskModel>, reverse?: boolean) {
-    if (reverse) {
-      this.taskView.next(tasks.reverse());
-    } else {
-      this.taskView.next(tasks);
-    }
+    this.modal
+      .open(TaskFormComponent, windowMode['defaultModal'])
+      .closed.subscribe((result: SkyModalCloseArgs) => {
+        this.service.save(Object.assign(new TaskModel(), result.data));
+      });
   }
 
 }
--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -3,55 +3,65 @@ import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/fires
 import { AuthService } from '../shared/auth.service';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { TaskModel } from './task.model';
+import { OrganizationModel } from '../shared/organization.model';
 import * as toolbox from 'sw-toolbox';
 
 @Injectable()
 export class TasksService {
-    public task$: BehaviorSubject<Array<TaskModel>> = new BehaviorSubject(undefined);
-    public taskCollection: AngularFirestoreCollection<TaskModel>;
+  public task$: BehaviorSubject<Array<TaskModel>> = new BehaviorSubject(new Array(new TaskModel()));
+  public tasksCollection: AngularFirestoreCollection<TaskModel>;
 
-    constructor(
-        private auth: AuthService,
-        private db: AngularFirestore) {
-        this.auth.org$.subscribe(org => this.getTaskBy(org.id));
-        this.tools();
-    }
-
-    private tools() {
-        // Setup SW Toolbox - verbose.
-        toolbox.options.debug = false;
-        toolbox.router.post('(.*)', toolbox.fastest);
-        toolbox.router.get('/tasks(.*)', toolbox.fastest, {
-            debug: false,
-            networkTimeoutSeconds: 4,
-            cache: {
-                name: 'tasks-cache-v1',
-                maxEntries: 10,
-                maxAgeSeconds: 200
-            }
+  constructor(
+    private auth: AuthService,
+    private db: AngularFirestore) {
+    this.auth.org$.subscribe((org: OrganizationModel) => {
+      this.tasksCollection = this.db.collection<TaskModel>('/organizations/' + org.id + '/tasks/');
+      this.tasksCollection.snapshotChanges().map(actions => {
+        return actions.map(action => {
+          const data = action.payload.doc.data() as TaskModel;
+          const id = action.payload.doc.id;
+          return { id, ...data };
         });
-    }
+      }).subscribe((data) => {
+        this.task$.next(data);
+        this.updateView(data, true);
+      });
+    });
+    this.tools();
+  }
 
-    private updateView(task: Array<TaskModel>, reverse?: boolean) {
-        if (reverse) {
-            this.task$.next(task.reverse());
-        } else {
-            this.task$.next(task);
-        }
+  public remove(id: string) {
+    this.tasksCollection.doc(id).delete();
+  }
+  public save(task: TaskModel) {
+    if (task.id) {
+      this.tasksCollection.doc(task.id).update(Object.assign({}, task));
+    } else {
+      this.tasksCollection.add(Object.assign({}, task));
     }
-    
-    private getTaskBy(orgId: string) {
-        if (orgId) {
-            // Connect to the database and get a list of task
-            this.taskCollection =
-                this.db.collection<TaskModel>('/organizations/' + orgId + '/tasks/');
-                this.taskCollection.snapshotChanges().map(actions => {
-                    return actions.map(action => {
-                        const data = action.payload.doc.data() as TaskModel;
-                        const id = action.payload.doc.id;
-                        return { id, ...data };
-                    });
-                }).subscribe(data => { this.updateView(data, true); });
-        }
+  }
+
+  private tools() {
+    // Setup SW Toolbox - verbose.
+    toolbox.options.debug = false;
+    toolbox.router.post('(.*)', toolbox.fastest);
+    toolbox.router.get('/tasks(.*)', toolbox.fastest, {
+      debug: false,
+      networkTimeoutSeconds: 4,
+      cache: {
+        name: 'tasks-cache-v1',
+        maxEntries: 10,
+        maxAgeSeconds: 200
+      }
+    });
+  }
+
+  private updateView(task: Array<TaskModel>, reverse?: boolean) {
+    if (reverse) {
+      this.task$.next(task.reverse());
+    } else {
+      this.task$.next(task);
     }
+  }
+
 }
