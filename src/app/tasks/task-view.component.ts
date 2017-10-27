e58e000b7c818aab984b82aa60a4b0b7c14d8a49
FireBat <FireBat>
2017-10-26 20:16:57 -0400

    add mouse move pointer to dashboard

--- src/app/tasks/task-view.component.ts
+++ src/app/tasks/task-view.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 import { TaskModel } from'./task.model';
 @Component({
     selector: 'uapi-task-view',
-    templateUrl: './task-view.component.html'
+    templateUrl: './task-view.component.html',
+    styleUrls: ['task-view.component.scss']
 })
 export class TaskViewComponent {
     @Input()
